311a5734984afb4eb8f052737307ddd2e0f0cbf5
KoenLam <47334582+KoenLam@users.noreply.github.com>
2021-05-06 17:12:05 +0200

    Trace: Added Shot Noise Model

--- simulator/trace.py
+++ simulator/trace.py
@@ -1,12 +1,6 @@
 import numpy as np
 import scipy.io
 import scipy.stats
-from config import YT_TRACE
-
-
-# M 1000000
-# N 100
-# s 0.8
 
 
 def gen_irm_trace(sample_size, catalog_size, power_law_exp):
@@ -17,14 +11,13 @@ def gen_irm_trace(sample_size, catalog_size, power_law_exp):
     samples = np.random.uniform(low=0, high=1, size=sample_size)
 
     # Create power_law distribution
-    power_law = np.arange(1, catalog_size+1)**(-power_law_exp)
-    steps = np.cumsum(power_law / sum(power_law))
+    # power_law = np.arange(1, catalog_size+1)**(-power_law_exp)
+    # steps = np.cumsum(power_law / sum(power_law))
 
-    # print(samples)
+    steps = gen_power_law_dist(catalog_size, power_law_exp)
 
     # Find the index of the sample in the power-law distribution 
-    # Note: in the highly unlikely case when the sample is exactly 1.0 this will crash
-    trace = [np.where(sample < steps)[0][0] for sample in samples] # [0][0] since the result of np.where is an tuple
+    trace = [np.where(sample <= steps)[0][0] for sample in samples] # [0][0] since the result of np.where is an tuple
 
     return np.array(trace)
 
@@ -32,22 +25,77 @@ def gen_dest_trace(sample_size, num_dest):
     return np.random.randint(low=0, high=num_dest, size=sample_size)
 
 
-
-def poisson_shot_model(shot_duration, pareto_pop_coeff, pareto_start, shot_rate, simulation_time=1000):
-    # A_pareto=gprnd(0.8,1.6,2,nof_shots,1); %generate shot height (popularities)
-    nof_shots = shot_rate*simulation_time
-    t = scipy.stats.genpareto.rvs(c=0.8, scale=1.6, loc=2, size=(nof_shots, 1))
-    print(t)
-
+def gen_power_law_dist(catalog_size, power_law_exp):
+    # Create power_law distribution
+    power_law = np.arange(1, catalog_size+1)**(-power_law_exp)
+    power_law_dist = np.cumsum(power_law / sum(power_law))
+    return power_law_dist
 
 
-def get_yt_trace():
+def shot_noise_model_matlab(shot_duration, shot_rate, simulation_time=1000, par_shape=0.8, par_scale=1.6, par_loc=2):
+    nof_shots = int(shot_rate*simulation_time)
+    files_mean_requests = scipy.stats.genpareto.rvs(c=par_shape, scale=par_scale, loc=par_loc, size=nof_shots)
+    files_shot_requests = scipy.stats.poisson.rvs(mu=files_mean_requests, size=nof_shots)
+
+    total_samples = sum(files_shot_requests)
+
+    trace_time = np.zeros(total_samples)
+    trace_requests = np.zeros(total_samples)
+
+    idx = 0
+    for file_id, file_shot_requests in enumerate(files_shot_requests):
+        trace_requests[idx:idx+file_shot_requests] = file_id
+        
+        shot_arrival = np.random.rand()*simulation_time
+        requests_arrival = np.random.rand(file_shot_requests)*shot_duration
+        trace_time[idx:idx+file_shot_requests] = shot_arrival + requests_arrival
+
+        idx += file_shot_requests
+    
+    snm_trace = trace_requests[np.argsort(trace_time)].astype(int)
+    return snm_trace
+
+
+
+def shot_noise_model(max_shot_duration, shot_rate, simulation_time=1000, par_shape=0.8, par_scale=1.6, par_loc=2):
+    nof_shots = int(shot_rate*simulation_time)
+    files_mean_requests = scipy.stats.genpareto.rvs(c=par_shape, scale=par_scale, loc=par_loc, size=nof_shots)
+    files_shot_requests = scipy.stats.poisson.rvs(mu=files_mean_requests, size=nof_shots)
+
+    total_samples = sum(files_shot_requests)
+
+    trace_time = np.zeros(total_samples)
+    trace_requests = np.zeros(total_samples)
+
+    idx = 0
+    for file_id, file_shot_requests in enumerate(files_shot_requests):
+        trace_requests[idx:idx+file_shot_requests] = file_id
+        
+        shot_arrival = np.random.rand()*simulation_time
+        shot_duration = np.random.rand()*max_shot_duration
+
+        requests_arrival = np.random.rand(file_shot_requests)*shot_duration
+        trace_time[idx:idx+file_shot_requests] = shot_arrival + requests_arrival
+
+        idx += file_shot_requests
+    
+    snm_trace = trace_requests[np.argsort(trace_time)].astype(int)
+    return snm_trace
+
+
+def get_yt_trace(yt_trace):
     # return load_mat_array(r"./traces/yt_trace.mat")['trace0']
-    return load_mat_array(YT_TRACE)['trace0']
+    return load_mat_array(yt_trace)['trace0']
     
 
 def load_mat_array(filename):
     return scipy.io.loadmat(filename, squeeze_me=True)
+
+
+def get_trace_stats(trace: np.ndarray):
+    sample_size = trace.size
+    catalog_size = np.max(trace)+1
+    return sample_size, catalog_size
 
 
 def parse_trace(mat_file : dict):
@@ -72,6 +120,6 @@ if __name__ == "__main__":
     # print(t)
     # yt = get_yt_trace()
 
-    poisson_shot_model(shot_duration=100, pareto_pop_coeff=1.25, pareto_start=2, shot_rate=0.1, simulation_time=1000)
+    shot_noise_model_matlab(shot_duration=100, shot_rate=0.1, simulation_time=1000)
 
     
